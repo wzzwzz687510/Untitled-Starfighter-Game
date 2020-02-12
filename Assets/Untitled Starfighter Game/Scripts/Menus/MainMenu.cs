81c9b8bb123e5038950b0193784b3632792f9232
MrMinks <47115849+MrMinks@users.noreply.github.com>
2020-02-12 11:22:54 +0000

    Implemented main menu (still needs some logic) & camera spin.
    Added in simple main menu that still needs some logic and a whole lotta spruce'in up. Although the camera spin is a nice touch :)

---
+++ Assets/Untitled Starfighter Game/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+  public GameObject mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+      mainCamera.transform.Rotate(0, Time.deltaTime, 0);
+    }
+
+    public void Quit()
+    {
+      UnityEditor.EditorApplication.isPlaying = false;
+		  Application.Quit();
+	  }
+}
