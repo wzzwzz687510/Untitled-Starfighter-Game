c8d8a4b62655e8bc5e7f2a506ceeca4879831fc4
MrMinks <47115849+MrMinks@users.noreply.github.com>
2020-03-11 19:00:50 +0000

    Correct UI Merge Commit. (Read desc)
    I've went through all existing scenes and everyhting is OK on my end - the game scene UI is still outdated  (not sure if you've implemented my new UI into that scene yet?) - but the UI-Test scene is fine and the Main-Menu scene is all ok but the ship model needs to be readded and reanimated due to current conflicts of the new model and materials assigned in the game scene (it was only temporary in the main menu scene anyway, so it doesn't really matter...)

---
+++ Assets/Untitled Starfighter Game/Scripts/Menus/MinimapLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapLogic : MonoBehaviour
+{
+    //References the player.
+    public Transform player;
+
+    //Gets called after Update and FixedUpdate.
+    void LateUpdate()
+    {
+      //Creates new position of the 'minimap' camera.
+      Vector3 newPosition = player.position;
+      //Keeps the new camera zoom position to the same set position.
+      newPosition.y = transform.position.y;
+      transform.position = newPosition;
+
+      //Rotates the camera with the player.
+      transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
